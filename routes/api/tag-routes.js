32c563062798d29d9accd732a955c0a7ca69645b
Michael Coder7 <michaelokikiade@yahoo.co.uk>
2023-08-12 12:15:20 +0100

    tag routes in progress

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -35,12 +35,35 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   // create a new tag
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagData = await Tag.update(
+      {
+        tag_name: req.body.tag_name,
+      },
+      { where: { id: req.params.id } }
+    );
+    if (!categoryData) {
+      res
+        .status(404)
+        .json({ message: "Oops, there are no results matching this id" });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 router.delete("/:id", (req, res) => {
